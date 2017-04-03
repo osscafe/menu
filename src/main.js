36fbb0a0b8b289b8b5c0ce26049666bbae755850
Tsutomu Kawamura <cognitom@gmail.com>
2017-04-03 15:12:10 +0900

    Auto reloading

--- src/main.js
+++ src/main.js
@@ -4,3 +4,7 @@ import './app.tag'
 import './menu-item.tag'
 
 riot.mount('app')
+
+// Reload every hour to get updates remotely
+const aHour = 1000 * 60 * 60
+window.setTimeout(() => window.reload(), aHour)
